9bc75ae06fb88119baec543f202180c62262a719
Yağmur AKTAŞ <72444387+rainwhitestone@users.noreply.github.com>
2022-08-11 18:08:43 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,116 @@
+
+//detecting button press
+
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfDrums ;i++){
+
+    document.querySelectorAll("button")[i].addEventListener("click",handleClick);
+}
+
+function handleClick (){
+
+   var buttonInnerHTML = this.innerHTML;
+
+   makeSound(buttonInnerHTML);
+
+   buttonAnimation(buttonInnerHTML);
+
+}
+
+// detecting keyboard press
+
+   document.addEventListener("keydown", function(event)
+   
+   {
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+
+   });
+
+   function makeSound(key) {
+
+
+    switch(key) {
+        case "w":
+            var audio = new Audio("/drumkit/sounds/tom-1.mp3");
+            audio.play();
+    
+        break;
+    
+    
+        case "a":
+            var audio = new Audio("/drumkit/sounds/tom-2.mp3");
+            audio.play();
+        
+        break;
+    
+    
+        case "s":
+            var audio = new Audio("/drumkit/sounds/tom-3.mp3");
+            audio.play();
+            
+        break;
+    
+    
+        case "d":
+            var audio = new Audio("/drumkit/sounds/tom-4.mp3");
+            audio.play();
+                
+        break;
+        
+        case "j":
+            var audio = new Audio("/drumkit/sounds/snare.mp3");
+            audio.play();
+                
+        break;
+    
+        
+        case "k":
+            var audio = new Audio("/drumkit/sounds/crash.mp3");
+            audio.play();
+                
+        break;
+    
+        case "l":
+            var audio = new Audio("/drumkit/sounds/kick-bass.mp3");
+            audio.play();
+                
+        break;
+
+        case "h":
+            var audio = new Audio("/drumkit/sounds/guitar.wav");
+            audio.play();
+                
+        break;
+
+    
+        default:
+            console.log(this.innerHTML);
+    
+       }
+
+   }
+
+  
+
+   function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+
+        activeButton.classList.remove("pressed");
+
+
+    }, 100);
+
+   }
+  
+    
+   
